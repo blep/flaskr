b8d17979b4b971ac4a41ff3a888d1efd0cf8c323
Khaled Mohamed Hassan <khaledmhassan1996@gmail.com>
2024-03-11 22:27:42 +0200

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,17 +5,15 @@ apt-get update
 
 apt install python3 python3-pip 
 
-Install Python dependencies using pip
-pip install Flask
-pip install pytest
-pip install requests
-
-ls
-pwd
+# Install Python dependencies using pip
+# pip install Flask
+# pip install pytest
+# pip install requests
 
 DEPLOYMENT_DIR="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
 
 # Change directory to the deployment bundle directory
 cd "$DEPLOYMENT_DIR" || exit 1  # Exit if the directory doesn't exist
-pwd
-ls
+pip install -r requirements.txt
+
+echo "beforeinstall success"
