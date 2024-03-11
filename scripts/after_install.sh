f212001231368e8dca00124f827b40b88cc494eb
Khaled Mohamed Hassan <khaledmhassan1996@gmail.com>
2024-03-11 22:32:36 +0200

    Create after_install.sh

---
+++ scripts/after_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DEPLOYMENT_DIR="/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive"
+
+# Change directory to the deployment bundle directory
+cd "$DEPLOYMENT_DIR" || exit 1  # Exit if the directory doesn't exist
+
+python manage.py run &
+
+echo "afterinstall success"
